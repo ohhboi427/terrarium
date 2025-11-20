ebc332a2093d22a7d9fbcd155c10bf88fb1f0ad6
Dávid Kunovics <kunovicsdavid@gmail.com>
2025-11-20 21:37:42 +0100

    CMake function for module creation

---
+++ cmake/TerrariumModule.cmake
@@ -0,0 +1,29 @@
+function(terrarium_add_module MODULE_NAME)
+    string(TOLOWER ${MODULE_NAME} MODULE_LOWER)
+    string(TOUPPER ${MODULE_NAME} MODULE_UPPER)
+
+    if(TERRARIUM_BUILD_SHARED)
+        set(SHARED_TARGET terrarium-${MODULE_LOWER}-shared)
+
+        add_library(${SHARED_TARGET} SHARED)
+        add_library(Terrarium::${MODULE_NAME} ALIAS ${SHARED_TARGET})
+
+        target_compile_definitions(${SHARED_TARGET}
+            PUBLIC
+            TERRA_BUILD_SHARED
+            PRIVATE
+            TERRA_EXPORT
+        )
+
+        set(TERRARIUM_${MODULE_UPPER}_TARGET ${SHARED_TARGET} PARENT_SCOPE)
+    endif()
+
+    if(TERRARIUM_BUILD_STATIC)
+        set(STATIC_TARGET terrarium-${MODULE_LOWER}-static)
+
+        add_library(${STATIC_TARGET} STATIC)
+        add_library(Terrarium::${MODULE_NAME} ALIAS ${STATIC_TARGET})
+
+        set(TERRARIUM_${MODULE_UPPER}_TARGET ${STATIC_TARGET} PARENT_SCOPE)
+    endif()
+endfunction()
